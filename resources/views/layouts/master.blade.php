3ed18f2d9ab8853b071a711d407d9cb4f9da3275
abdessamad jalal <a.jalal@enim.ac.ma>
2022-01-02 01:43:03 +0100

    admin

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,16 +8,16 @@
 
     <title>Auction</title>
 
-    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/assets/css/all.min.css">
-    <link rel="stylesheet" href="/assets/css/animate.css">
-    <link rel="stylesheet" href="/assets/css/nice-select.css">
-    <link rel="stylesheet" href="/assets/css/owl.min.css">
-    <link rel="stylesheet" href="/assets/css/magnific-popup.css">
-    <link rel="stylesheet" href="/assets/css/flaticon.css">
-    <link rel="stylesheet" href="/assets/css/jquery-ui.min.css">
-    <link rel="stylesheet" href="/assets/css/main.css">
-    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="{{asset("/assets/css/bootstrap.min.css")}}">
+    <link rel="stylesheet" href="{{asset("/assets/css/all.min.css")}}">
+    <link rel="stylesheet" href="{{asset("/assets/css/animate.css")}}">
+    <link rel="stylesheet" href="{{asset("/assets/css/nice-select.css")}}">
+    <link rel="stylesheet" href="{{asset("/assets/css/owl.min.css")}}">
+    <link rel="stylesheet" href="{{asset("/assets/css/magnific-popup.css")}}">
+    <link rel="stylesheet" href="{{asset("/assets/css/flaticon.css")}}">
+    <link rel="stylesheet" href="{{asset("/assets/css/jquery-ui.min.css")}}">
+    <link rel="stylesheet" href="{{asset("/assets/css/main.css")}}">
+    <link rel="shortcut icon" href="{{asset("assets/images/favicon.png")}}" type="image/x-icon">
     @yield('stylesheets')
 </head>
 
@@ -362,20 +362,20 @@
 
 
 
-    <script src="./assets/js/jquery-3.3.1.min.js"></script>
-    <script src="./assets/js/modernizr-3.6.0.min.js"></script>
-    <script src="./assets/js/plugins.js"></script>
-    <script src="./assets/js/bootstrap.min.js"></script>
-    <script src="./assets/js/isotope.pkgd.min.js"></script>
-    <script src="./assets/js/wow.min.js"></script>
-    <script src="./assets/js/waypoints.js"></script>
-    <script src="./assets/js/nice-select.js"></script>
-    <script src="./assets/js/counterup.min.js"></script>
-    <script src="./assets/js/owl.min.js"></script>
-    <script src="./assets/js/magnific-popup.min.js"></script>
-    <script src="assets/js/yscountdown.min.js"></script>
-    <script src="assets/js/jquery-ui.min.js"></script>
-    <script src="./assets/js/main.js"></script>
+    <script src="{{asset("./assets/js/jquery-3.3.1.min.js")}}"></script>
+    <script src="{{asset("./assets/js/modernizr-3.6.0.min.js")}}"></script>
+    <script src="{{asset("./assets/js/plugins.js")}}"></script>
+    <script src="{{asset("./assets/js/bootstrap.min.js")}}"></script>
+    <script src="{{asset("./assets/js/isotope.pkgd.min.js")}}"></script>
+    <script src="{{asset("./assets/js/wow.min.js")}}"></script>
+    <script src="{{asset("./assets/js/waypoints.js")}}"></script>
+    <script src="{{asset("./assets/js/nice-select.js")}}"></script>
+    <script src="{{asset("./assets/js/counterup.min.js")}}"></script>
+    <script src="{{asset("./assets/js/owl.min.js")}}"></script>
+    <script src="{{asset("./assets/js/magnific-popup.min.js")}}"></script>
+    <script src="{{asset("assets/js/yscountdown.min.js")}}"></script>
+    <script src="{{asset("assets/js/jquery-ui.min.js")}}"></script>
+    <script src="{{asset("./assets/js/main.js")}}"></script>
     @yield('scripts')
 </body>
 
