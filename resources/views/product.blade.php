989a892e2dc4af281fb19d456bbbe5a06283d2e7
abdessamad jalal <a.jalal@enim.ac.ma>
2022-01-02 02:42:57 +0100

    admin

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -867,6 +867,12 @@
     <script src="{{asset('/assets/js/main.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
+<script>
+    Weglot.initialize({
+        api_key: 'wg_ba2dcb24c7e21c809d78f25e62e5476e2'
+    });
+</script>
 </body>
 
 
